943fde487437c2b49d6d0c02cb7d9cee76582de9
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2019-03-05 11:46:10 -0600

    Bump version to 1.1.8

--- spec/lib/legacy_sendgrid_spec.rb
+++ spec/lib/legacy_sendgrid_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'LegacySendGrid' do
   it 'should have a version' do
-    expect(LegacySendGrid::VERSION).to eq('1.1.7')
+    expect(LegacySendGrid::VERSION).to eq('1.1.8')
   end
 end
